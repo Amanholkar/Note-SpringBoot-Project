d1763307a59a735a832a185e581220143b6b9da8
AmanDev <aman@techfrolic.com>
2022-07-25 11:30:14 +0530

    first commit

---
+++ note_rest_application/src/main/java/com/aman/noteapplication/note_rest_application/NoteRestApplication.java
@@ -0,0 +1,13 @@
+package com.aman.noteapplication.note_rest_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteRestApplication.class, args);
+	}
+
+}
---
+++ note_rest_application/src/test/java/com/aman/noteapplication/note_rest_application/NoteRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.noteapplication.note_rest_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
